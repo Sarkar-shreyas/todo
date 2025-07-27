4d6f9341b9004a8cdeb631c0da5fed201d365a18
Sarkar-shreyas <ssarkar20001@gmail.com>
2025-07-27 04:36:16 +0100

    Remade console program into a simple CLI app that logs tasks into a .json file, added 'clear all' option

--- todo_app.py
+++ todo_app.py
@@ -1,135 +1,178 @@
+import argparse as ap
+import os
+import json
+import sys
 TICK = "✓"
 BOX = "□"
-TASKS = []
+#Set up the parser
+parser = ap.ArgumentParser(description="A simple todo-list cli app.")
 
-#Simple options menu
-def menu():
-    print("1. Add a task")
-    print("2. Remove a task")
-    print("3. Mark task as complete")
-    print("4. Mark task as incomplete")
-    print("5. Show complete tasks")
-    print("6. Show incomplete tasks")
-    print("7. Show all tasks")
-    print("8. Quit")
+#Add arguments to accept
+parser.add_argument("command", help="add, del, mc (mark complete), mic (mark incomplete), lc (list complete), lic (list incomplete), l (list all), cl (clear all), q (quit)")
+parser.add_argument("task", nargs="?", default="", help="Task description or number")
+
+args = parser.parse_args()
+
+#Load tasks from file
+def loadTasks():
+    if os.path.exists("tasks.json"):
+        with open("tasks.json", "r") as file:
+            return json.load(file)
+    return []
+
+#Save tasks to file
+def saveTasks(tasks):
+    with open("tasks.json", "w") as file:
+        json.dump(tasks, file, indent=2)
+
 
 #Add a task to the list
 def addTask(task):
-    print("Please enter the task description: ")
-    newTask = input()
-    task.append(f"{newTask} {BOX}")
-    print("Your task was successfully added!")
-    print("What would you like to do next?")
-    return task
+    tasks = loadTasks()
+    if not task == "":
+        tasks.append(f"{BOX} {task}")
+        saveTasks(tasks)
+        print(f"'{task}' has been successfully added.")
+    else:
+        print("This task has no description")
+        print("Usage: python todo_app.py add 'Task description'")
+        return
 
 #Remove a task from the list
-def removeTask(task):
-    if not task:
-        return "The task list is currently empty!"
-    showTasks(task)
-    print("Enter which task you would like to remove: ")
-    remove = input()
-    while not remove.isdigit():
-        print("Please enter the corresponding task number:")
-        remove = input()
-    while int(remove) > len(task):
-        print("Please enter a valid number!\n")
-        remove = input()
+def removeTask(index):
+    tasks = loadTasks()
+    if not tasks:
+        print("There are no tasks currently logged.")
+    else:
+        if not index.isdigit():
+            print("You must enter a number.")
+            print("Usage: python todo_app.py del 'Task number'")
+            return
+        else:
+            taskindex = int(index)-1
+            if 0 <= taskindex < len(tasks):
+                task = tasks[taskindex]
+                tasks.pop(taskindex)
+                saveTasks(tasks)
+                print(f"'{task}' has been successfully removed.")
+            else:
+                print("Invalid index.")
 
-    del task[int(remove) - 1]
-    return f"Task {remove} has been successfully deleted!"
+#Remove all tasks from the list
+def clearTasks():
+    tasks = loadTasks()
+    if not tasks:
+        print("There are no tasks currently logged.")
+    else:
+        tasks.clear()
+        saveTasks(tasks)
+        print("All tasks have been removed.")
 
 #Mark a task as complete
-def markComplete(task):
-    if not task:
-        return "The task list is currently empty!"
-    showTasks(task)
-    print("Which task has been completed?")
-    taskNum = input()
-    while int(taskNum) > len(task):
-        print("Please enter a valid number!")
-        taskNum = input()
-
-    task[int(taskNum)-1] = task[int(taskNum)-1].replace(BOX, TICK)
-    return f"Congratulations! Task {taskNum} has been completed!"
+def markComplete(index):
+    tasks = loadTasks()
+    if not index.isdigit():
+        print("You must enter a number.")
+        print("Usage: python todo_app.py mc 'Task number'")
+        return
+    else:
+        taskindex = int(index)-1
+        if 0 <= taskindex < len(tasks):
+            if BOX in tasks[taskindex]:
+                tasks[taskindex] = tasks[taskindex].replace(BOX, TICK)
+                saveTasks(tasks)
+                print(f"'{tasks[taskindex]}' is now complete.")
+            else:
+                print("That task is already complete.")
+        else:
+            print("Invalid index.")
 
 #Mark a task as incomplete
-def markIncomplete(task):
-    if not task:
-        return "The task list is currently empty!"
-
-    showTasks(task)
-    print("Which task do you want to mark incomplete?")
-    taskNum = input()
-    while int(taskNum) > len(task):
-        print("Please enter a valid number!")
-        taskNum = input()
-    if BOX in task[int(taskNum)-1]:
-        return "That task is already marked incomplete!"
-
-    task[int(taskNum)-1] = task[int(taskNum)-1].replace(TICK, BOX)
-    return f"Task {taskNum} has been marked incomplete!"
-
-
-#Print out all currently logged tasks
-def showTasks(task, num=0):
-    if not task:
-        print("The task list is currently empty!")
+def markIncomplete(index):
+    tasks = loadTasks()
+    if not index.isdigit():
+        print("You must enter a number.")
+        print("Usage: python todo_app.py mic 'Task number'")
         return
-    if num == 0:
-        print("Would you like to see the current list of tasks?\nEnter Y or N:")
-        show = input()
-        while show.lower() != "y" and show.lower() != "n":
-            print("Please enter Y or N:")
-            show = input()
-        if show.lower() == "y":
-            for item in task:
-                print(f"{task.index(item)+1}. {item}")
-    elif num == 7:
-        print("This is the list of current tasks:")
-        for item in task:
-            print(f"{task.index(item) + 1}. {item}")
-    elif num == 6:
-        print("This is the list of incomplete tasks:")
-        for item in task:
-            if BOX in item:
-                print(f"{task.index(item) + 1}. {item}")
-    elif num == 5:
-        print("This is the list of completed tasks:")
-        for item in task:
-            if TICK in item:
-                print(f"{task.index(item) + 1}. {item}")
-
-
-def main():
-
-    print("Welcome to the todo-list app!")
-    while True:
-        print("What would you like to do?")
-        menu()
-        print("Enter your choice: ")
-        choice = input()
-
-        if choice == '8':
-            print("Thank you for using the todo-list app! :)")
-            return
-        elif choice == '1':
-            addTask(TASKS)
-        elif choice == '2':
-            removeTask(TASKS)
-        elif choice == '3':
-            markComplete(TASKS)
-        elif choice == '4':
-            markIncomplete(TASKS)
-        elif choice == '5':
-            showTasks(TASKS, int(choice))
-        elif choice == '6':
-            showTasks(TASKS, int(choice))
-        elif choice == '7':
-            showTasks(TASKS, int(choice))
+    else:
+        taskindex = int(index)-1
+        if 0 <= taskindex < len(tasks):
+            if TICK in tasks[taskindex]:
+                tasks[taskindex] = tasks[taskindex].replace(TICK, BOX)
+                saveTasks(tasks)
+                print(f"'{tasks[taskindex]}' is now incomplete.")
+            else:
+                print("That task is already incomplete.")
         else:
-            print("Please enter a valid choice: ")
-            choice = input()
+            print("Invalid index.")
+
+
+#Print all tasks
+def listTasks():
+    tasks = loadTasks()
+    if not tasks:
+        print("There are no tasks currently logged.")
+    else:
+        for i, task in enumerate(tasks, start=1):
+            print(f"{i}. {task}")
+
+#Print all completed tasks
+def listComplete():
+    tasks = loadTasks()
+    if not tasks:
+        print("There are no tasks currently logged.")
+    else:
+        if not any(TICK in task for task in tasks):
+            print("There are no complete tasks.")
+        else:
+            for i, task in enumerate(tasks, start=1):
+                if TICK in task:
+                    print(f"{i}. {task}")
+
+#Print all incomplete tasks
+def listIncomplete():
+    tasks = loadTasks()
+    if not tasks:
+        print("There are no tasks currently logged.")
+    else:
+        if not any(BOX in task for task in tasks):
+            print("All tasks are complete.")
+        else:
+            for i, task in enumerate(tasks, start=1):
+                if BOX in task:
+                    print(f"{i}. {task}")
+
+#Main body
+def main():
+    #Fetch command and task
+    command = args.command.lower()
+    task = args.task
+
+    #Respond to each command
+    match command:
+        case 'q' | 'quit':
+            print("Thank you for using the todo-list app! :)")
+            sys.exit()
+        case 'add':
+            addTask(task)
+        case 'del' | 'delete':
+            removeTask(args.task)
+        case 'mc' | 'mark complete':
+            markComplete(args.task)
+        case 'mic' | 'mark incomplete':
+            markIncomplete(args.task)
+        case 'lc' | 'list complete':
+            #showTasks(tasks, int(choice))
+            listComplete()
+        case 'lic' | 'list incomplete':
+            #showTasks(tasks, int(choice))
+            listIncomplete()
+        case 'l' | 'list':
+            listTasks()
+        case 'cl' | 'clear':
+            clearTasks()
+        case _:
+            print("Invalid command.")
 
 if __name__ == "__main__":
     main()
